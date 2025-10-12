8c80888564744dedb2fbe2653e09ed85dcacd1cb
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2025-10-12 04:02:48 +0000

    push commit and try deploy trigger

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="inline bg-red-600 text-4xl text-yellow-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
