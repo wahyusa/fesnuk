f7f48acc9caee9ec39eabdf23b651bf4652fad51
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2025-10-12 05:06:55 +0000

    chore: try ssr x spa show local date with rune

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1 class="inline bg-red-600 text-4xl text-yellow-400">Welcome to SvelteKit</h1>
-<p class="inline bg-red-600 text-xl text-yellow-400">
-	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
-</p>
+<script lang="ts">
+	let date = $state(new Date().toLocaleString());
+
+	$effect(() => {
+		const interval = setInterval(() => {
+			date = new Date().toLocaleString();
+		}, 1000);
+
+		return () => clearInterval(interval);
+	});
+</script>
+
+<time>{date}</time>
+<h1>Fesnuk</h1>
