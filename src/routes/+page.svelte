244cfb11ca06c3b45e71746cc851fab4d6cee15f
Wahyu SA <27491616+wahyusa@users.noreply.github.com>
2025-10-14 08:12:08 +0000

    chore setup fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,18 @@
 	});
 </script>
 
-<time>{date}</time>
-<h1>Fesnuk</h1>
-<a href="#!" aria-label="Link to development version preview of this website">View dev version</a>
+<header>
+	<nav>Home</nav>
+	<section id="hero">Hero</section>
+</header>
+
+<main>
+	<section id="background">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit id eos ab quod at odio
+		necessitatibus explicabo autem, natus labore et tempore. Incidunt unde consequatur vitae
+		corrupti totam non dicta.
+		<button type="button">
+			<span class="font-sketch"> Really? </span>
+		</button>
+	</section>
+</main>
